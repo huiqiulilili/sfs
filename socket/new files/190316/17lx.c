68efe7a72b730b47584399444fa81257ac1a1752
huiqiulilili <48478957+huiqiulilili@users.noreply.github.com>
2019-03-28 19:37:52 +0800

    lx2019年3月28日

---
+++ socket/new files/190316/17lx.c
@@ -0,0 +1,103 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+//移位操作符
+//int main()
+//{
+//	int a = 10;
+//	int b;
+//	b = a << 2;
+//	printf("b = %d\n", b);
+//	system("pause");
+//	return 0;
+//}
+//位操作符
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = a | b;
+//	printf("c = %d\n", c);
+//	system("pause");
+//	return 0;
+//}
+//赋值操作符
+//int main()
+//{
+//	int a = 10;
+//	a <<= 1;
+//	printf("%d\n", a);
+//	system("pause");
+//	return 0;
+//}
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#define SSS "hahahaha"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Math.h>
+//int main()
+//{
+//	printf("%d", sizeof(short));//2
+//	printf("%d", sizeof(int));//4
+//	printf("%d", sizeof(long));//4
+//	printf("%d", sizeof(char));//1
+//	printf("%d", sizeof(long long));//8
+//	printf("%d", sizeof(float));//4
+//	printf("%d", sizeof(double));//8
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d %d", strlen(SSS), sizeof(SSS));
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	char ch;
+//	ch = getchar();
+//	while (ch != '\0')
+//	{
+//		if (ch == ' ')
+//		{
+//			putchar(ch);
+//		}
+//		else
+//		{
+//			putchar(ch + 1);
+//		}
+//		ch = getchar();
+//	}
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	char ch;
+//	ch = getchar();
+//	putchar(ch);
+//	system("pause");
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	char ch;
+//
+//	ch = getchar();
+//	putchar(ch);
+//	putchar(ch);
+//	ch = getchar();
+//	putchar(ch);
+//	putchar(ch);
+//	ch = getchar();
+//	putchar(ch);
+//	putchar(ch);
+//	putchar(ch);
+//	system("pause");
+//	return 0;
+//}
