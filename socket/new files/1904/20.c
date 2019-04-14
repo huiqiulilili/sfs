05412be7e28e39c9b9b2ab523de59d858f6fd8d8
huiqiulilili <48478957+huiqiulilili@users.noreply.github.com>
2019-04-14 20:03:10 +0800

    lx 2019年4月14日

---
+++ socket/new files/1904/20.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+//int main()
+//{
+//	int n = 10;
+//	char *pc = &n;
+//	int *pi = &n;
+//	printf("%p\n", &n);
+//	printf("%p\n", pc);
+//	printf("%p\n", pc + 1);
+//	printf("%p\n", pi);
+//	printf("%p\n", pi + 1);
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	char n = 10;
+//	char *pc = &n;
+//	int *pi = &n;
+//	printf("%p\n", &n);
+//	printf("%p\n", pc);
+//	printf("%p\n", pc + 1);
+//	printf("%p\n", pi);
+//	printf("%p\n", pi + 1);
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int n = 0x11223344;
+//	int *p = &n;
+//	char *p2 = p;
+//	*p2 = 0x55;
+//	printf("%x\n", *p);
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int n = 0x11223344;
+//	int *p = &n;
+//	char *p1 = p;
+//	char *p2 = p;
+//	*p1 = 0x55;
+//	*p2 = 0;
+//	printf("%x\n", *p);
+//	system("pause");
+//	return 0;
+//}
