0eb77e1e92481d9e4721d3fe53b404893fd9284b
huiqiulilili <48478957+huiqiulilili@users.noreply.github.com>
2019-04-22 18:01:41 +0800

    lx 2019年4月22日

---
+++ socket/new files/190401/04.c
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+//将三个数按从大到小输出。
+//int main()
+//{
+//	int a = 10;
+//	int b = 120; 
+//	int tmp;
+//	int c = 30;
+//	/*if (a < b)
+//	{
+//		tmp = a;
+//		a = b;
+//		b = tmp;
+//	}
+//	if (a < c)
+//	{
+//		tmp = a;
+//		a = c;
+//		c = tmp;
+//	}
+//	if (b < c)
+//	{
+//		tmp = b;
+//		b = c;
+//		c = tmp;
+//	}*/
+//	printf("%d %d %d \n", a, b, c);
+//	system("pause");
+//	return 0;
+//}
+//求两个数的最大公约数。
+//int main()
+//{
+//	int a = 6;
+//	int b = 12;
+//	int max = (a > b ? a : b);
+//	if (max % a == 0 && max % b == 0)
+//	{
+//		printf("最大公约数为%d\n", max);
+//	}
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 6;
+//	int b = 12;
+//	int max = (a < b ? a : b);
+//	if (a % max == 0 && b % max == 0)
+//	{
+//		printf("最小公倍数为%d\n", max);
+//	}
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int tmp;
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+//	int arr1[10] = { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+//	int i;
+//	for (i = 0; i < 9; i++)
+//	{
+//		tmp = arr[i];
+//		arr[i] = arr1[i + 9];
+//		arr1[i + 9] = tmp;
+//	}
+//	system("pause");
+//	return 0;
+//}
